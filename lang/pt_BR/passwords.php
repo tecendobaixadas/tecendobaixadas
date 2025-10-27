65169dc12eada777e6eb7bc85b725062c89e813b
Cauã <cauaserpa092@gmail.com>
2025-10-27 14:32:13 -0300

    [feat] traduz mensagens de erro em Login

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de linguagem de redefinição de senha
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são as mensagens padrão que correspondem
+    | aos motivos fornecidos pelo corretor de senhas para uma tentativa
+    | de atualização de senha que falhou.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida com sucesso!',
+    'sent' => 'Enviamos um link para redefinir sua senha no seu e-mail!',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não encontramos um usuário com esse endereço de e-mail.",
+
+];
