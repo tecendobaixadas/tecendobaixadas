15d26741e44e55d810650818918148a1a269693a
Cauã <cauaserpa092@gmail.com>
2025-10-27 14:21:48 -0300

    [feat] traduz mensagens de erro em Login

---
+++ lang/pt_BR/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de linguagem de validação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas contêm as mensagens de erro padrão usadas pela
+    | classe validadora. Algumas dessas regras possuem múltiplas versões,
+    | como as regras de tamanho. Sinta-se à vontade para ajustar cada uma
+    | dessas mensagens aqui.
+    |
+    */
+
+    'failed'               => 'Senha ou e-mail incorreto.',
+    'password'             => 'A senha informada está incorreta.',
+    'throttle'             => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+    'unauthorized'         => 'Você não tem permissão para acessar este recurso.',
+    'logout'               => 'Você saiu da sua conta com sucesso.',
+
+];
